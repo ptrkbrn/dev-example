9170e9728a09ea328f38085177f2609e8a73d05e
Patrick Breen <pressure78@gmail.com>
2022-03-16 13:14:35 -0400

    add unique keys to listings. comment code. delete vestigal console.logs.

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import './App.css';
 
 function App() {
   const [listings, setListings] = useState([]);
+  // on page load, fetches first 3 item listings
   useEffect(() => {
     fetch('https://gemlux.com/collections/rigging-kits/products.json')
       .then((res) => res.json())
       .then((data) => {
+        // sets item listings in state
         setListings(data.products.slice(0, 3));
       });
   }, []);
-  console.log(`listings: ${listings}`);
   return (
     <div className="App">
       <h1>Rigging Kits</h1>
       <div className="container">
-        {listings.map((listing) => <Listing title={listing.title} image={listing.images[0].src} />)}
+        {listings.map((listing) => (
+          <Listing
+            title={listing.title}
+            image={listing.images[0].src}
+            key={listing.id}
+          />
+        ))}
       </div>
     </div>
   );
